3b83da620c6685fb70d2b4dba817ad8a73c4dd48
Tevis Lucas <lubaking.co@gmail.com>
2026-03-14 16:10:48 +0100

    Delete gallery directory

--- gallery/wp-content/plugins/wpstars-helpers/wpstars-helpers.js
+++
@@ -1,9 +0,0 @@
-/* www.metaconsulting.net - Martin Sternsberger */
-/* Helper functions */
-
-// Open all PDF-files in a new tab
-jQuery.noConflict();
-
-jQuery(document).ready(function($){
-	$("a[href$='pdf']").attr('target','_blank');
-});
